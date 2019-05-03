0e5fa6109106c4650ca291815ac3da9a2ac4527a
UCA <you@example.com>
2019-05-03 07:04:39 -0600

    Initial commit

---
+++ app/src/main/java/com/example/evlab2_pdm_00011417/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.evlab2_pdm_00011417
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
